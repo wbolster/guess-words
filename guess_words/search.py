726c2aec6ecaa9ea8d15b1d0f9a7e715f1a44ab0
Wouter Bolsterlee <uws@xs4all.nl>
2014-06-03 22:27:16 +0200

    Split into cli and search module

--- guess_words/search.py
+++ guess_words/search.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python3
-
-import argparse
 import collections
 import itertools
-
-
-DEFAULT_DICT_FILE = '/usr/share/dict/words'
 
 
 def word_to_sorted_anagram(word):
@@ -37,30 +31,3 @@ class Searcher(object):
                 answers.update(value)
 
         return answers
-
-
-def main():
-    # Parse command line.
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dict', default=DEFAULT_DICT_FILE,
-                        help="path to word list file")
-    parser.add_argument('n_letters', type=int,
-                        help="number of letter in word to guess")
-    parser.add_argument('letters', help="all possible letters")
-    args = parser.parse_args()
-
-    with open(args.dict) as fp:
-        searcher = Searcher(fp)
-
-    answers = searcher.lookup(args.letters, args.n_letters)
-
-    if not answers:
-        print("No answers found.")
-    else:
-        print("Possible answers:")
-        for n, word in enumerate(sorted(answers), 1):
-            print("{:3d}. {}".format(n, word))
-
-
-if __name__ == '__main__':
-    main()
