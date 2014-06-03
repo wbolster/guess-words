898fe9ea557c45290715bec2be3fdddbcebf6b58
Wouter Bolsterlee <uws@xs4all.nl>
2014-06-03 22:20:48 +0200

    Refactor into a Searcher class

--- guess.py
+++ guess.py
@@ -13,6 +13,32 @@ def word_to_sorted_anagram(word):
     return ''.join(sorted(word))
 
 
+class Searcher(object):
+    """Searcher class"""
+
+    def __init__(self, fp):
+        # Build a lookup table by making each word's sorted anagram point to
+        # the original word.
+        self.table = collections.defaultdict(list)
+        for line in fp:
+            word = line.strip().lower()
+            key = word_to_sorted_anagram(word)
+            self.table[key].append(word)
+
+    def lookup(self, letters, n_letters):
+        # Try all combinations of the input. Consult the lookup table for
+        # each combo to see whether it points to a known word (or multiple
+        # known words).
+        answers = set()
+        for combo in itertools.combinations(letters, n_letters):
+            key = word_to_sorted_anagram(combo)
+            value = self.table.get(key)
+            if value is not None:
+                answers.update(value)
+
+        return answers
+
+
 def main():
     # Parse command line.
     parser = argparse.ArgumentParser()
@@ -23,31 +49,16 @@ def main():
     parser.add_argument('letters', help="all possible letters")
     args = parser.parse_args()
 
-    # Build a lookup table by making each word's sorted anagram point to
-    # the original word.
-    words_by_anagram = collections.defaultdict(list)
     with open(args.dict) as fp:
-        for line in fp:
-            word = line.strip().lower()
-            key = word_to_sorted_anagram(word)
-            words_by_anagram[key].append(word)
+        searcher = Searcher(fp)
 
-    # Try all combinations of the input. Consult the lookup table for
-    # each combo to see whether it points to a known word (or multiple
-    # known words).
-    possible_answers = set()
-    for combo in itertools.combinations(args.letters, args.n_letters):
-        key = word_to_sorted_anagram(combo)
-        value = words_by_anagram.get(key)
-        if value is not None:
-            possible_answers.update(value)
+    answers = searcher.lookup(args.letters, args.n_letters)
 
-    # Produce nice output.
-    if not possible_answers:
+    if not answers:
         print("No answers found.")
     else:
         print("Possible answers:")
-        for n, word in enumerate(sorted(possible_answers), 1):
+        for n, word in enumerate(sorted(answers), 1):
             print("{:3d}. {}".format(n, word))
 
 
